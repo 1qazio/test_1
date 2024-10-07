d62cf37d4531158cfc311d8df6c044f8745d2124
1qazio <63549716+1qazio@users.noreply.github.com>
2024-10-07 20:49:09 +0300

    Add files via upload

---
+++ Test_1/Test_1/Test_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int vivod1() {
+	std::cout << rewerge;
+}
+
+int main() {
+	"dvjdsbjdfbvj"
+		1 fdvfd
+		2 fsfsgf
+		3 fdd
+		4 fgfd
+		5 dfgdf
+
+
+		switch (switch_on)
+		{
+		case 1:
+			vivod1()
+		default:
+			std::cout<< "error"
+			break;
+		}
+}
